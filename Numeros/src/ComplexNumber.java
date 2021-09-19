bd7ea57c2960058707b8eb70aedc2e960595f675
Daniel <danicabe@gmail.com>
2021-09-19 13:49:56 +0200

    test 100% codigo 50%

--- Numeros/src/ComplexNumber.java
+++ Numeros/src/ComplexNumber.java
@@ -19,7 +19,8 @@ public class ComplexNumber {
      */
     private double realpart, imagpart;
     ComplexNumber(double a, double b){
-        
+        realpart = a;
+        imagpart = b;
     }
     
     /**
@@ -27,7 +28,7 @@ public class ComplexNumber {
      * @return the real part of the ComplexNumber
      */
     public double realPart(){
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.realpart;
     }
     
     /**
@@ -35,7 +36,7 @@ public class ComplexNumber {
      * @return the imaginary part of the ComplexNumber
      */
     public double imaginaryPart(){
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.imagpart;
     }
     
     /**
@@ -47,8 +48,8 @@ public class ComplexNumber {
      *        this + c
      */
     public ComplexNumber add (ComplexNumber c){
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        return new ComplexNumber(this.realpart+c.realpart, this.imagpart + c.imagpart);
+      }
     
     /**
      * Returns the result of subtracting c from the ComplexNumber
@@ -59,7 +60,7 @@ public class ComplexNumber {
      *        this - c
      */
     public ComplexNumber subtract (ComplexNumber c){
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new ComplexNumber(this.realpart-c.realpart, this.imagpart - c.imagpart);
     }
     
     /**
@@ -71,8 +72,6 @@ public class ComplexNumber {
      *          this * c2
      */
     public ComplexNumber multiply (ComplexNumber c){
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-
     }
     
    /**
@@ -109,4 +108,27 @@ public class ComplexNumber {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 
     } 
+
+   
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ComplexNumber other = (ComplexNumber) obj;
+        if (Double.doubleToLongBits(this.realpart) != Double.doubleToLongBits(other.realpart)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.imagpart) != Double.doubleToLongBits(other.imagpart)) {
+            return false;
+        }
+        return true;
+    }
+
 }
