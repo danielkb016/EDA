8807da8fcd6ed78a513b5bcd67c8cb8fde89a00b
Daniel <danicabe@gmail.com>
2021-10-19 12:02:31 +0200

    19/10/21

---
+++ hashCode/src/hashcode/Alumno.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashcode;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author DANI
+ */
+public class Alumno {
+    private final String nombre,apellido;
+    private final int DNI;
+
+    public Alumno(String nombre, String apellido, int DNI) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+  
+
+}
---
+++ hashCode/src/hashcode/HashCode.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashcode;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author DANI
+ */
+public class HashCode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HashMap<Integer,Alumno> mapa = new HashMap<>();
+    }
+    
+}
